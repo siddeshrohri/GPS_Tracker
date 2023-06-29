99b738a1dc47f3b815c994b7c60b7d699c038381
Siddesh R Ohri <64972996+siddeshrohri@users.noreply.github.com>
2023-06-29 14:07:57 +0100

    GPS tracker exception cases

---
+++ GPSException.java
@@ -0,0 +1,10 @@
+/**
+ * Represents a GPS-related exception.
+ *
+ * @author Nick Efford
+ */
+public class GPSException extends RuntimeException {
+  public GPSException(String message) {
+    super(message);
+  }
+}
